360da0c0d0f6397bbc9b87566d0277e9302f22d8
LAPTOP-CDGB0N8P\user <haikalazzrial345@gmail.com>
2024-02-25 20:22:47 +0700

    hobby.kt

---
+++ app/src/main/java/com/example/aplikasikotlin/biodata/hobby.kt
@@ -0,0 +1,27 @@
+package com.example.aplikasikotlin.biodata
+
+fun main(args: Array<String>) {
+    biodata("Haikal Azzrial Akbar", "hal hal yang memiliki rasa asin")
+    hobby("Game", "Saya suka bermain Mobile Legend kalau belum immortal belum istirahat")
+}
+
+fun biodata(nama: String, makananKesukaan: String, kelas: String = "2A") {
+    println("""
+        ------------------------------------------------------------------
+        =                             Hello                              =
+        ------------------------------------------------------------------
+        Nama saya $nama. Saya sekarang kuliah di Politeknik Negeri Madiun
+        kelas $kelas. Makanan Favorit saya adalah $makananKesukaan
+    """.trimIndent())
+    print("\n")
+}
+
+fun hobby(nama: String, deskripsi: String) {
+    println("""
+        =================================================================
+        |                              Hobi                   
+        =================================================================
+        Hobi        : $nama
+        Deskripsi   : $deskripsi
+    """.trimIndent())
+}
